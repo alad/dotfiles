9a75d0595efcd3fc4a9412e50cd38441e4f4e0ef
Abhimanyu Lad <abhimanyulad@gmail.com>
2013-05-13 00:00:23 -0700

    zsh custom key bindings

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -5,6 +5,15 @@
 bindkey -v                                          # Use vi key bindings
 bindkey '^r' history-incremental-search-backward    # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
 
+bindkey '^k' vi-cmd-mode
+
+bindkey '^f' history-substring-search-up
+bindkey '^g' history-substring-search-down
+
 # emacs style
 bindkey '^a' beginning-of-line
 bindkey '^e' end-of-line
+
+bindkey "^x^e" edit-command-line
+bindkey "^b" backward-word
+bindkey '^d' kill-word
